3b35df9b0fce3a564293efd98a26540f299a54da
pramos <pramos.corelogic.pl>
2016-05-25 11:53:55 +0200

    Get ads perfect

--- app/src/main/java/uk/co/ribot/Knacket/ui/adapter/AdAdapter.java
+++ app/src/main/java/uk/co/ribot/Knacket/ui/adapter/AdAdapter.java
@@ -8,12 +8,13 @@ import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
 import java.io.IOException;
-import java.net.MalformedURLException;
+import java.io.InputStream;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
@@ -22,13 +23,15 @@ import javax.inject.Inject;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import de.hdodenhof.circleimageview.CircleImageView;
-import retrofit2.http.Url;
+import io.fabric.sdk.android.services.concurrency.AsyncTask;
 import uk.co.ribot.Knacket.R;
 import uk.co.ribot.Knacket.data.local.model.AdDatabase;
 import uk.co.ribot.Knacket.ui.main.AdInfo;
 
 public class AdAdapter extends RecyclerView.Adapter<AdAdapter.BuyerViewHolder> {
     private List<AdDatabase> mAd;
+    private ImageView imageView;
+    private Bitmap bitmap;
 
     @Inject
     public AdAdapter() {
@@ -49,33 +52,25 @@ public class AdAdapter extends RecyclerView.Adapter<AdAdapter.BuyerViewHolder> {
     public void onBindViewHolder(BuyerViewHolder holder, int position) {
         AdDatabase ad = mAd.get(position);
         holder.touchArea.setTag(ad.getId());
+        imageView = holder.ivProfilePic;
 
-        /*if(!TextUtils.isEmpty(ad.getUserProfile().getPicture_url())) {  GET PROFILE PIC, PROBLEMS WITH URL
-            URL url = null;
-            try {
-                url = new URL(ad.getUserProfile().getPicture_url());
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            }
+        if (!TextUtils.isEmpty(ad.getUserProfile().getPicture_url())) {
+            String SERVER_URL = "http://37.139.26.18:12347/uploads/";
+            String url = (SERVER_URL + ad.getUserProfile().getPicture_url());
+            new LoadImage().execute(url);
+        }
 
-            Bitmap image = null;
-            try {
-                image = BitmapFactory.decodeStream(url.openConnection().getInputStream());
-                holder.ivProfilePic.setImageBitmap(image);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }*/
-
-        holder.tvBuyerName.setText(ad.getUser().getName());
-        holder.tvAdCategory.setText(ad.getTag().getName());
-        holder.tvAdDate.setText(ad.getTime());
-        holder.tvAdDesc.setText(ad.getText());
-
-        if(!TextUtils.isEmpty(ad.getPrice()))
+        if (!TextUtils.isEmpty(ad.getUser().getName()))
+            holder.tvBuyerName.setText(ad.getUser().getName());
+        if (!TextUtils.isEmpty(ad.getTag().getName()))
+            holder.tvAdCategory.setText(ad.getTag().getName());
+        if (!TextUtils.isEmpty(ad.getTime()))
+            holder.tvAdDate.setText(ad.getTime());
+        if (!TextUtils.isEmpty(ad.getText()))
+            holder.tvAdDesc.setText(ad.getText());
+        if (!TextUtils.isEmpty(ad.getPrice()))
             holder.tvAdPrice.setText(ad.getPrice());
-
-        if(!TextUtils.isEmpty(ad.getUser().getRating()))
+        if (!TextUtils.isEmpty(ad.getUser().getRating()))
             holder.rbAdRating.setRating(Float.parseFloat(ad.getUser().getRating()));
 
         holder.touchArea.setOnClickListener(new View.OnClickListener() {
@@ -105,6 +100,22 @@ public class AdAdapter extends RecyclerView.Adapter<AdAdapter.BuyerViewHolder> {
         public BuyerViewHolder(View itemView) {
             super(itemView);
             ButterKnife.bind(this, itemView);
+        }
+    }
+
+    private class LoadImage extends AsyncTask<String, String, Bitmap> {
+        @Override
+        protected Bitmap doInBackground(String... args) {
+            try {
+                bitmap = BitmapFactory.decodeStream((InputStream)new URL(args[0]).getContent());
+            } catch (IOException e) { e.printStackTrace(); }
+
+            return bitmap;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap image) {
+            if(image != null) imageView.setImageBitmap(image);
         }
     }
 }
